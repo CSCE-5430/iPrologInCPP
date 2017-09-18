df10cace3bd46c93063ba6ed71757c99b6cf5cd5
khaira0206 <khaira0206@gmail.com>
2017-09-18 17:16:04 -0500

    initial compilable file

---
+++ Main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Main{
+
+public: static void println(){
+
+cout<<"/";
+}
+
+public: static void pp(){
+cout<<"/";
+
+}
+
+public: static void run(const char* fname0){
+
+std::string fname = fname0 ;
+fname.append(".nl");
+}
+
+public: static void srun(const char* fname0){
+std::string fname = fname0 ;
+fname.append(".nl");
+}
+};
+int main(int argc,const char* argv[]){
+const char* fname = argv[0];
+Main ob;
+ob.run(fname);
+return 0;
+
+}
+
