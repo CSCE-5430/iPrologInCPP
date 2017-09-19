0e35571c191308a290f60f78c703d428c7406eda
Khorrom <mdkhorromkhan@my.unt.edu>
2017-09-18 23:11:12 -0500

    Added Indentation

--- Main.cpp
+++ Main.cpp
@@ -4,32 +4,32 @@ using namespace std;
 
 class Main{
 
-public: static void println(){
+    public: static void println(){
 
-cout<<"/";
-}
+        cout<<"/";
+    }
 
-public: static void pp(){
-cout<<"/";
+    public: static void pp(){
+        cout<<"/";
 
-}
+    }
 
-public: static void run(const char* fname0){
+    public: static void run(const char* fname0){
 
-std::string fname = fname0 ;
-fname.append(".nl");
-}
+        string fname = fname0 ;
+        fname.append(".nl");
+    }
 
-public: static void srun(const char* fname0){
-std::string fname = fname0 ;
-fname.append(".nl");
-}
+    public: static void srun(const char* fname0){
+        string fname = fname0 ;
+        fname.append(".nl");
+    }
 };
 int main(int argc,const char* argv[]){
-const char* fname = argv[0];
-Main ob;
-ob.run(fname);
-return 0;
+    const char* fname = argv[0];
+    Main ob;
+    ob.run(fname);
+    return 0;
 
 }
 
