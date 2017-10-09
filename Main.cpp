4d6678b256cdf65c4bb1e76fac146a572087f0f0
imraneeecuet <imran.eee.cuet.com>
2017-10-09 22:33:17 +0600

    Removing Java Sources into another branch

--- Main.cpp
+++ Main.cpp
@@ -4,33 +4,34 @@ using namespace std;
 
 class Main{
 
-    public: static void println(){
-
+    public: static void println()
+    {
         cout<<"/";
     }
 
-    public: static void pp(){
+    public: static void pp()
+    {
         cout<<"/";
-
     }
 
-    public: static void run(const char* fname0){
-
+    public: static void run(const char* fname0)
+    {
         string fname = fname0 ;
         fname.append(".nl");
     }
 
-    public: static void srun(const char* fname0){
+    public: static void srun(const char* fname0)
+    {
         string fname = fname0 ;
         fname.append(".nl");
     }
 };
 
-int main(int argc,const char* argv[]){
+int main(int argc,const char* argv[])
+{
     const char* fname = argv[0];
     Main ob;
     ob.run(fname);
     return 0;
-
 }
 
