10b5049109e7ca0303094d774a4c8fffe6911325
Khorrom <mdkhorromkhan@my.unt.edu>
2017-11-16 12:53:22 -0600

    test

---
+++ IntList.cpp
@@ -0,0 +1,69 @@
+#include "IntList.h"
+
+
+IntList :: IntList(const int head) 
+{
+  this.head = head;
+  tail = null;
+}
+
+IntList :: IntList(const int X, const IntList Xs) 
+{
+  head = X;
+  tail = Xs;
+}
+
+const boolean IntList :: isEmpty(const IntList Xs) 
+{
+  return null == Xs;
+}
+
+const int IntList :: head(const IntList Xs) 
+{
+  return Xs.head;
+}
+
+const IntList empty = null;
+
+const IntList IntList :: tail(const IntList Xs) 
+{
+  return Xs.tail;
+}
+
+const IntList IntList :: cons(const int X, const IntList Xs) 
+{
+  return new IntList(X, Xs);
+}
+
+const IntList IntList :: app(const int[] xs, const IntList Ys) 
+{
+  IntList Zs = Ys;
+  for (int i = xs.length - 1; i >= 0; i--) 
+  {
+    Zs = cons(xs[i], Zs);
+  }
+  return Zs;
+}
+
+const IntStack IntList :: toInts(IntList Xs) 
+{
+  const IntStack is = new IntStack();
+  while (!isEmpty(Xs)) 
+  {
+    is.push(head(Xs));
+    Xs = tail(Xs);
+  }
+  return is;
+}
+
+const int IntList :: len(const IntList Xs) 
+{
+  return toInts(Xs).size();
+}
+
+virtual String IntList :: toString() 
+{
+  return toInts(this).toString();
+}
+
+
---
+++ IntStack.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+
+using namespace std;
+
+class IntStack
+{
+  public:
+    int top;
+  static int SIZE;
+
+    int *stack;
+  static int MINSIZE ;
+
+  public: IntStack()
+  {
+    stack = new int[SIZE];
+    int i;
+    for(i = 0; i < SIZE; i++)
+    {
+      stack[i] = 0;
+    }
+    top = -1;
+  }
+
+  public :IntStack(int size)
+  {
+    stack = new int[size];
+    int i ;
+    for(i = 0; i < size; i++)
+    {
+      stack[i] = 0;
+    }
+    top = -1;
+  }
+
+  const int getTop()
+  {
+    return top;
+  }
+
+  const int setTop(const int top)
+  {
+    return this->top = top;
+  }
+
+  const void clear()
+  {
+    top = -1;
+  }
+
+  const bool isEmpty()
+  {
+    return top < 0;
+  }
+
+  void push(int i)
+  {
+    if(++top >= SIZE)
+    {
+      expand();
+    }
+    stack[top] = i;
+  }
+
+  const int pop()
+  {
+    const int r = stack[top--];
+    shrink();
+    return r;
+  }
+
+  const int get(const int i)
+  {
+    return stack[i];
+  }
+
+  const void set(const int i, const int val)
+  {
+    stack[i] = val;
+  }
+
+  const int size()
+  {
+    return top + 1;
+  }
+
+  private : const void expand()
+  {
+    int l = SIZE;
+    SIZE = SIZE <<1;
+    int *newstack = new int[SIZE];
+    for(int i = 0; i < l; i++)
+    {
+      newstack[i] = stack[i];
+      cout<< newstack[i]<< endl;
+    }
+    delete[] stack;
+    stack = newstack;
+  }
+
+  private: const void shrink()
+  {
+    int l = SIZE;
+    if(l <= MINSIZE || top << 2 >= 1)
+    {
+      return;
+    }
+    l = 1 + (top << 1);
+    if(top < MINSIZE)
+    {
+      l = MINSIZE;
+    }
+    int *newstack1 = new int[l];
+    for(int i = 0; i < l; i++)
+    {
+      newstack1[i] = stack[i];
+      delete[] stack;
+      stack = newstack1;
+    }
+  }
+  
+  public: void print()
+  {
+    for(int i=0; i< SIZE; i++)
+    {
+      cout<< stack[i]<< endl;
+    }
+  }
+
+};
+
+int IntStack :: SIZE = 16;
+int IntStack :: MINSIZE = 1 << 15;
+
+int main()
+{
+  IntStack intstack;
+  return 0;
+}
